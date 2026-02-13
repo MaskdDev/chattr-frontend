fc143aa628766ed7dcb63443783e33bb9df77caf
MaskdDev <maskddev@gmail.com>
2026-02-14 00:50:31 +1100

    Redirect to rooms page when logged in and opening root domain

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import HomePageHero from "@/components/HomePageHero";
+import { useAuth } from "@/app/providers/AuthProvider";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
+  // Use auth
+  const { session } = useAuth();
+
+  // If user is logged in, redirect to rooms page
+  if (session !== null) {
+    redirect("/rooms");
+  }
+
+  // Return component
   return (
     <div className="h-dvh overflow-y-hidden bg-black">
       <HomePageHero />
