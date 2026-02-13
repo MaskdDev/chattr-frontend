cf9583b9103e5eb2c60c22c85c6f17d4cc7367e7
MaskdDev <maskddev@gmail.com>
2026-02-13 12:07:10 +1100

    Set authClient refetch session options

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -5,6 +5,10 @@ import { usernameClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   plugins: [passkeyClient(), usernameClient()],
+  sessionOptions: {
+    refetchOnWindowFocus: false,
+    refetchInterval: 3 * 60 * 1000,
+  },
 });
 
 export type Session = typeof authClient.$Infer.Session;
